67055b346200d6e694a5c112fccc26708da41508
amirehsan <ehsansolout@gmail.com>
2025-02-23 11:15:09 +0330

    Add Db connection.

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"wa-service/config"
+	"wa-service/models"
 )
 
 func main() {
 	cfg := config.NewConfig()
 	fmt.Println(cfg)
+	err := models.ConnectDB()
+	println(err)
+
 }
--- models/db.go
+++ models/db.go
@@ -2,18 +2,25 @@ package models
 
 import (
 	"database/sql"
+	"log"
+	"wa-service/config"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// DB is a package-level variable that holds the database connection.
 var DB *sql.DB
 
-func ConnectDatabase() error {
-	db, err := sql.Open("sqlite3", "./names.db")
+// ConnectDB connects to the database and assigns the connection to DB.
+func ConnectDB() error {
+	var err error
+	DB, err = sql.Open("sqlite3", config.AppConfig.DB_PATH)
 	if err != nil {
 		return err
 	}
-
-	DB = db
+	if err = DB.Ping(); err != nil {
+		return err
+	}
+	log.Println("✅ Database connected successfully")
 	return nil
 }
